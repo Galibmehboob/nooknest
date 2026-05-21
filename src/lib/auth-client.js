df0db6b8215142539ebfe55b0c0e340ae1800d64
Galib Mehboob <86629449+Galibmehboob@users.noreply.github.com>
2026-05-21 09:11:13 +0600

    deploy issue fixing

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,20 @@
+// import { createAuthClient } from "better-auth/react";
+
+// export const authClient = createAuthClient({
+//     baseURL: "http://localhost:3000",
+// });
+
+// export const {
+//     useSession,
+//     signIn,
+//     signUp,
+//     signOut,
+// } = authClient;
+
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.NEXT_PUBLIC_APP_URL,
 });
 
 export const {
